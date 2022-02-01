5545a6a8f1817708450984e5ee10da993d467b55
marcorubini <malorubi@outlook.com>
2022-02-01 23:28:30 +0100

    test json library

--- test/01-json_compiles.cpp
+++ test/01-json_compiles.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 int main ()
-{}
+{
+  auto json = nlohmann::json::parse ("[0, 1, 2, 42]");
+
+  std::cout << "Printing json \"[0, 1, 2, 42]\"" << std::endl;
+  std::cout << json.dump () << std::endl;
+}
